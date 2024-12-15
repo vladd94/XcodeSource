25f9486b438fa2b2b42b7b9f900bf6472855424e
vladd94 <finch-06.bluish@icloud.com>
2024-12-15 12:52:52 -0600

    Initial Commit

---
+++ XcodeSource/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XcodeSource
+//
+//  Created by Vlad Stoicoviciu on 12/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XcodeSourceTests/XcodeSourceTests.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeSourceTests.swift
+//  XcodeSourceTests
+//
+//  Created by Vlad Stoicoviciu on 12/15/24.
+//
+
+import Testing
+@testable import XcodeSource
+
+struct XcodeSourceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
